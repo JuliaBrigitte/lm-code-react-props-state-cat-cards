2d266c176b6d70420e3974489e89588e448a9127
Julia Branke <Julia.Branke@gmx.co.uk>
2022-04-05 22:41:47 +0100

    activity 6 done, which data in App.tsx

---
+++ src/data/cat_image_description.ts
@@ -0,0 +1,9 @@
+export default interface CatImageDescription
+{
+    image:              string
+    altText:            string
+    licenceType:        string
+    licenceUrl:         string
+    attributionName:    string
+    attributionUrl:     string
+}
